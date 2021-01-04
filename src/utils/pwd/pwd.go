58f77e5c73ad8db6102e1f77e304bd9ac241d0e2
guzhouchao <guzhouchao@supremind.com>
2021-01-04 20:05:34 +0800

    update Encrypt Key

--- src/utils/pwd/pwd.go
+++ src/utils/pwd/pwd.go
@@ -4,12 +4,19 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"encoding/hex"
+	"os"
 )
 
 var (
 	commonIV = []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f}
 	keyText  = []byte("********************************")
 )
+
+func init()  {
+	if key:=os.Getenv("HUAWEI_CSI_KEY");key!=""{
+		keyText=[]byte(key)
+	}
+}
 
 //加密
 func Encrypt(password string) (string, error) {
